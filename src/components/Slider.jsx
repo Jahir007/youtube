17a8a1fa5cfa538e01f2cc7ce2bed30403697ce0
Jahir007 <jk584853@gmail.com>
2022-05-13 11:24:05 +0530

    master

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,6 +6,7 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    position: relative;
 `
 
 const Arrow = styled.div`
@@ -13,16 +14,60 @@ const Arrow = styled.div`
     height: 50px;
     background-color: #fff7f7;
     border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === 'left' && '10px'};
+    right: ${props => props.direction === 'right' && '10px'};
+    margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
 `
+
+
+const Wrapper = styled.div`
+    height: 100%;
+`
+
+
+const Slide = styled.div`
+    display: flex;
+    align-items: center;
+
+`
+
+const ImgContainer = styled.div`
+    flex: 1;
+`
+
+const Image = styled.div`
+
+`
+
+
+const InfoContainer = styled.div`
+    flex: 1;
+`
+
 
 
 const Slider = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction="left">
             <ArrowLeftOutlined />
         </Arrow>
-        <Arrow>
+        <Wrapper>
+            <ImgContainer>
+                <Image src="https://blog.bvblogic.com/wp-content/uploads/2020/02/shopping-apps-1280x723.jpg" />
+            </ImgContainer>
+            <InfoContainer></InfoContainer>
+
+        </Wrapper>
+        <Arrow direction="right">
             <ArrowRightOutlined />
         </Arrow>
     </Container>
