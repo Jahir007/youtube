6e109de4abb13a1450291d534fd85f2a9ee50e9e
Jahir007 <jk584853@gmail.com>
2022-05-17 11:36:01 +0530

    master

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,7 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import { useState } from 'react'
 import styled from 'styled-components'
+import { sliderItems } from '../data'
 
 
 const Container = styled.div`
@@ -7,6 +9,7 @@ const Container = styled.div`
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -25,53 +28,102 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${props => props.slideIndex * -100}vw);
 `
 
 
 const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
     display: flex;
     align-items: center;
-
+    background-color: #${props => props.bg};
 `
 
 const ImgContainer = styled.div`
+    height: 100%;
     flex: 1;
 `
 
-const Image = styled.div`
-
+const Image = styled.img`
+    height: 80%;
 `
 
 
 const InfoContainer = styled.div`
     flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Desc = styled.p`
+    margin: 50px  0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 
 
 const Slider = () => {
-  return (
-    <Container>
-        <Arrow direction="left">
-            <ArrowLeftOutlined />
-        </Arrow>
-        <Wrapper>
-            <ImgContainer>
-                <Image src="https://blog.bvblogic.com/wp-content/uploads/2020/02/shopping-apps-1280x723.jpg" />
-            </ImgContainer>
-            <InfoContainer></InfoContainer>
 
-        </Wrapper>
-        <Arrow direction="right">
-            <ArrowRightOutlined />
-        </Arrow>
-    </Container>
-  )
+    const [slideIndex, setSlideIndex] = useState(0)
+
+    const handleClick = (direction) => {
+
+        if(direction === 'left') {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2 )
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0 )
+        }
+
+    };
+
+    return (
+        <Container>
+            <Arrow direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map(item => (
+
+                    <Slide bg={item.bg} key={item.id}> 
+                        <ImgContainer>
+                            <Image src={item.img} />
+                        </ImgContainer>
+                        <InfoContainer>
+                            <Title>{item.title}</Title>
+                            <Desc>{item.desc}</Desc>
+                            <Button>
+                                SHOW NOW
+                            </Button>
+                        </InfoContainer>
+                    </Slide>
+                ))}
+            </Wrapper>
+            <Arrow direction="right" onClick={() => handleClick("right")}>
+                <ArrowRightOutlined />
+            </Arrow>
+        </Container>
+    )
 }
 
 export default Slider
